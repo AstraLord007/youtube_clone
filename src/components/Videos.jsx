ff9a1dfd3894220dba70d9bb3f6cf6905c73347b
Mauricio Dorado Lenz <mauridlenz@gmail.com>
2023-07-05 16:44:47 -0300

    VideoDetail component done.

--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -2,7 +2,7 @@ import { Stack, Box } from '@mui/material';
 import { ChannelCard, VideoCard } from "./";
 
 const Videos = ({ videos, direction }) => {
-    //   if(!videos?.length) return <Loader />;
+      if(!videos?.length) return 'Loading...';
     console.log(videos)
     return (
         <Stack direction={direction || "row"} flexWrap="wrap" justifyContent="start" alignItems="start" gap={2}>
